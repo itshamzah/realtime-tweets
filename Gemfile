4230d75d198b35f07bfdf7753452124ea35c3dff
Hamza Zubair <gondal.humza@gmail.com>
2020-08-25 18:10:54 +0200

    Env Variables and Bootstrap

--- Gemfile
+++ Gemfile
@@ -1,3 +1,5 @@
+gem 'dotenv'
+
 source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
@@ -52,3 +54,9 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+
+#GEMS
+gem 'bootstrap-sass'
+
+
--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
 require_relative 'boot'
 
 require 'rails/all'
+require 'dotenv'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -10,6 +11,9 @@ module Streamer
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+
+    #Load ENV variables
+	Dotenv.load
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
