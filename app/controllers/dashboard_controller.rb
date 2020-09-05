e702c5a550924c5d868c9636af8c88c2abe4cab0
Hamza Zubair <gondal.humza@gmail.com>
2020-09-05 20:10:06 +0200

    Pagination on Wall and Timeline

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -8,11 +8,12 @@ class DashboardController < ApplicationController
 
 	def index
     # REST client to make API calls for logged in user
-    twitter_rest_client = TwitterFactory.new_rest_client( @user[:token], @user[:secret] )
+    twitter_rest_client = TwitterFactory.new_rest_client(@user[:token], @user[:secret])
 
-    @tweets = twitter_rest_client.user_timeline(page: 1, count: 20).take(20)
+    @tweets = twitter_rest_client.user_timeline(tweet_params.symbolize_keys).take(20)
     @tweets.map! { |tweet| TwitterFactory::Tweet.new(tweet) }
 
+    set_page
     respond_to do |format|
       format.html
       format.json { render json: @tweets }
@@ -23,9 +24,10 @@ class DashboardController < ApplicationController
 		# REST client to make API calls for logged in user
     twitter_rest_client = TwitterFactory.new_rest_client( @user[:token], @user[:secret] )
 
-    @tweets = twitter_rest_client.home_timeline(page: 1, count: 20).take(20)
+    @tweets = twitter_rest_client.home_timeline(tweet_params.symbolize_keys)
     @tweets.map! { |tweet| TwitterFactory::Tweet.new(tweet) }
 
+    set_page
     respond_to do |format|
       format.html
       format.json { render json: @tweets }
@@ -38,5 +40,24 @@ class DashboardController < ApplicationController
     # Logged in user setup
     @user = current_user
     redirect_to root_path unless @user
+  end
+
+  def set_page
+    @page_number = params[:page].nil? ? 1 : params[:page] .to_i
+  end
+
+  def default_params
+    { page: 1, count: 20 }
+  end
+
+  def check_valid
+    params.merge({ count: 20 })
+    return params if (params[:page] && params[:page].to_i.between?(1, 4))
+
+    params.merge(default_params)
+  end
+
+  def tweet_params
+    check_valid.permit(:page, :count).to_h
   end
 end
