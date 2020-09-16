97f5843ea133723cd04b00c8f5738108edeb5eb7
Shairyar Baig <shairyarbaig@gmail.com>
2020-09-16 13:40:33 +0500

    setup rspec

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -4,6 +4,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @user = User.from_omniauth(request.env["omniauth.auth"])
 
     if @user.persisted?
+
       sign_in_and_redirect @user, event: :authentication
       set_flash_message(:notice, :success, kind: "Twitter") if is_navigational_format?
     else
